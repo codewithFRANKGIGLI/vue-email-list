b452f3004f37ceac0cae4b8bd31a5a00dc13ee9b
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-04-09 17:18:25 +0200

    feat: v-for e function mails

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,23 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue! :)',
-            otherMessage: 'Ciao ciao'
+            arrayMails: []
         };
+    },
+    methods: {
+        getMailsFromApi() {
+            // Chiamiamo l'api che genera una main
+            // quando otteniamo la risposta: popoliamo la variabile arrayMails col
+            // la mail random ottenua dall'API
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
+            then((response) => {
+                const serverData = response.data;
+                this.arrayMails.push(serverData.response);
+            });
+        }
+    },
+    mounted() {
+        this.getMailsFromApi();
     }
 }).mount('#app');
+
